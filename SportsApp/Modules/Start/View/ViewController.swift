7afd7fe5706a938173f4bca6089f25c21e61d619
Albaraa Alsayed <albaraaalsayed4@gmail.com>
2026-05-23 17:17:46 +0300

    first commit

---
+++ SportsApp/Modules/Start/View/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  SportsApp
+//
+//  Created by albaraa alsayed on 06/12/1447 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
