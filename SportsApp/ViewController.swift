8f803709ddaa70cc25cf7ba13670fbfb8da04e38
Albaraa Alsayed <albaraaalsayed4@gmail.com>
2026-05-23 13:59:28 +0300

    Initial Commit

---
+++ SportsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportsApp
+//
+//  Created by albaraa alsayed on 06/12/1447 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
