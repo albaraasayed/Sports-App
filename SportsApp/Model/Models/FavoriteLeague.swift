a0aa13eeb798834100b1b72f895a16a268e5d706
Albaraa Alsayed <albaraaalsayed4@gmail.com>
2026-06-05 19:49:22 +0300

    Add Presenter for league and leagues Details and Teams

--- SportsApp/Model/Models/FavoriteLeague.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-struct FavoriteLeague {
-    let leagueId: String
-    let name: String
-    let logo: String
-    let sport: String // e.g., "Football", "Basketball"
-}
---
+++ SportsApp/Modules/TeamDetails/Presneter/TeamDetailsPresenter.swift
@@ -0,0 +1,47 @@
+//
+//  TeamDetailsPresenter.swift
+//  SportsApp
+//
+//  Created by albaraa alsayed on 19/12/1447 AH.
+//
+
+import Foundation
+
+protocol TeamDetailsPresenterProtocol: AnyObject {
+    func viewDidLoad()
+}
+
+class TeamDetailsPresenter: TeamDetailsPresenterProtocol {
+    
+    weak var view: TeamDetilsVCProtocol?
+    var sportType: SportType
+    var teamId: String
+    
+    init(view: TeamDetilsVCProtocol, sportType: SportType, teamId: String) {
+        self.view = view
+        self.sportType = sportType
+        self.teamId = teamId
+    }
+    
+    func viewDidLoad() {
+        view?.showLoading()
+        
+        NetworkManager.shared.fetchTeam(sport: sportType, teamId: teamId) { [weak self] result in
+            guard let self = self else { return }
+            
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let teams):
+                    if let firstTeam = teams.first, let players = firstTeam.players {
+                        self.view?.hideLoaPlayers(players: players, teamName: firstTeam.teamName ?? "Unknown Team")
+                    } else {
+                        self.view?.showError(message: "No players found for this team.")
+                    }
+                    
+                case .failure(let error):
+                    self.view?.showError(message: error.localizedDescription)
+                }
+            }
+        }
+    }
+}
