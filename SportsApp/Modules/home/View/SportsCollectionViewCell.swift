116e1e83a41154badcfabccf35bdfd6dbe5f7d4e
Mahmoud Tarek <tmahmoud137@gmail.com>
2026-06-05 18:49:56 +0300

    Add Home Views

---
+++ SportsApp/Modules/home/View/SportsCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SportsCollectionViewCell.swift
+//  SportsApp
+//
+//  Created by albaraa alsayed on 06/12/1447 AH.
+//
+
+import UIKit
+
+class SportsCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var sportLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.layer.borderColor = UIColor.Primary.cgColor
+        self.layer.borderWidth = 1
+        self.layer.cornerRadius = 18
+    }
+    
+    
+
+}
