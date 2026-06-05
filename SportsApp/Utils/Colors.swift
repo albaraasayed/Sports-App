e29009ec93795357a6c4cdf85e7cc36af79885ac
Mahmoud Tarik <tmahmoud137@gmail.com>
2026-06-05 19:19:22 +0300

    Add Utils Folder (Colors and Fonts)

---
+++ SportsApp/Utils/Colors.swift
@@ -0,0 +1,20 @@
+//
+//  Colors.swift
+//  SportsApp
+//
+//  Created by albaraa alsayed on 06/12/1447 AH.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let Primary = UIColor(named: "Primary")!
+    static let Secondary = UIColor(named: "Secondary")!
+    
+    static let AppBackground = UIColor(named: "AppBackground")!
+    static let SurfaceGray = UIColor(named: "SurfaceGray")!
+    
+    static let TextPrimary = UIColor(named: "TextPrimary")!
+    static let TextSecondary = UIColor(named: "TextSecondary")!
+}
